9638b38d1483cb18ea3019a7149e2aadd0187e3e
LGY <ae911@qq.com>
2021-11-14 18:53:59 +0800

    Initial Commit

---
+++ pta/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pta
+//
+//  Created by E A on 2021/11/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
